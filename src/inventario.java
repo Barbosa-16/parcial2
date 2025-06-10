13a2838b31f5d9b34b04f0abcfb45ed378aa3000
daya <ceilinbarbosa16@gmail.com>
2025-06-10 07:28:35 -0500

    examen final, en el repo puse disque parcial y no se cambiar el nombre, minimo un 1 por eso todo bien

--- src/inventario.java
+++ src/inventario.java
@@ -1,11 +1,33 @@
-
+import java.util.Scanner;
 
 public class inventario {
     public static void main(String[] args) {
-        int [][] matriz = new int[i][j];
-        for (int i = 0; i < ; i++) {
-
-        }
-    }
+        int [][] matriz = new int[10][10];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce los valores para la matriz 10x10:");
+        for (int fila = 0; fila < 10; fila++) {
+            for (int columna = 0; columna < 10; columna++) {
+                System.out.print("Elemento [" + fila + "][" + columna + "]: ");
+                matriz[fila][columna] = input.nextInt();
+            }
+   public static int contarProductosAgotados(int[][] matriz) {
     
+
+   }
+
+public static double promedioInventarioBajo(int[][] matriz) {
+
+}
+
+public static void imprimirInventarioCritico(int[][] matriz) {
+
+}
+
+        
+public static int[] posicionMayorInventario(int[][] matriz) {
+
+}
+
+}
+    }
 }
