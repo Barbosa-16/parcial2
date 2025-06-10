888ebca847a232d698dbbe378d5f68c897d4a945
daya <ceilinbarbosa16@gmail.com>
2025-06-10 07:03:33 -0500

    tengame piedad jajaja y al menos pongame un 1

---
+++ src/inventario.java
@@ -0,0 +1,11 @@
+
+
+public class inventario {
+    public static void main(String[] args) {
+        int [][] matriz = new int[i][j];
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
+    
+}
